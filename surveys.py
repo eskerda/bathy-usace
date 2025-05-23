b767b291c67237a9c3ac25ed0387e61611bfe300
eskerda <eskerda@gmail.com>
2025-05-23 11:27:07 +0200

    parse args in surveys query script
    Add a bunch of command line args to the surveys querying
    script.

--- surveys.py
+++ surveys.py
@@ -3,6 +3,7 @@
 import csv
 import sys
 import json
+import argparse
 
 import requests
 from tqdm import tqdm
@@ -12,18 +13,23 @@ PAGE_SIZE = 100
 ENDPOINT = "https://services7.arcgis.com/n1YM8pTrFmm7L4hs/arcgis/rest/services/eHydro_Survey_Data/FeatureServer/0/query"
 
 
-def arcgis_q(offset=0, count=100, q="1=1", **kwargs):
+def arcgis_q(query="1=1", offset=0, count=100, order=None, **kwargs):
     args = {
         "f": "json",
         "resultOffset": offset,
         "resultRecordCount": count,
-        "where": q,
+        "where": query,
         "outFields": "*",
         "resultType": "standard",
         "returnGeometry": False,
-        "orderByFields": "OBJECTID",
+        "orderByFields": order or "OBJECTID",
     }
-    return requests.get(ENDPOINT, params=dict(args, **kwargs)).json()
+    r = requests.get(ENDPOINT, params=dict(args, **kwargs)).json()
+
+    if 'error' in r:
+        raise Exception(r)
+
+    return r
 
 
 def arcgis_q_count(*args, **kwargs):
@@ -31,20 +37,53 @@ def arcgis_q_count(*args, **kwargs):
     return r["count"]
 
 
-q = "1=1"
-# q = "(usacedistrictcode='CENAN') AND (channelareaidfk='CENAN_JI_01_INL')"
-total = arcgis_q_count(q=q)
+def main(args):
+    if args.outfile and args.outfile != '-':
+        out = open(args.outfile, 'w')
+    else:
+        out = sys.stdout
 
-writer = csv.DictWriter(sys.stdout, fieldnames={})
+    writer = csv.DictWriter(out or sys.stdout, fieldnames={})
 
-# XXX use asyncio to parallelize
-# XXX move to main()
-# XXX use argparse
-for i in tqdm(range(0, total, PAGE_SIZE)):
-    surveys = [f["attributes"] for f in arcgis_q(i, PAGE_SIZE, q=q)["features"]]
+    queries = []
+    if args.query:
+        queries.append(f"({args.query})")
+    if args.district:
+        queries.append(f"(usacedistrictcode='{args.district}')")
+    if args.channel:
+        queries.append(f"(channelareaidfk='{args.channel}')")
+    query = ' AND '.join(queries)
+    query = query or "1=1"
 
-    if i == 0:
-        writer.fieldnames = sorted(surveys[0].keys())
-        writer.writeheader()
+    total = arcgis_q_count(query, order=args.order)
+    total = min(args.total or total, total)
+    page = min(total, PAGE_SIZE)
 
-    writer.writerows(surveys)
+    # XXX use asyncio to parallelize
+    for i in tqdm(range(0, total, page)):
+        surveys = [
+            f["attributes"] for f in
+                arcgis_q(query, i, page, order=args.order)["features"]
+        ]
+
+        if i == 0:
+            writer.fieldnames = sorted(surveys[0].keys())
+            writer.writeheader()
+
+        writer.writerows(surveys)
+
+    if out:
+        out.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', dest='outfile', help='CSV output file')
+    parser.add_argument('-n', dest='total', help='limit to N surveys', default=0, type=int)
+    parser.add_argument('--query', dest='query', help='ArcGIS filter query')
+    parser.add_argument('--district', dest='district', help='filter by district code (ex: CENAN)')
+    parser.add_argument('--channel-area-id', dest='channel', help='filter by channel area id (ex: CENAN_JI_01_INL)')
+    parser.add_argument('--order-by', dest='order', help='order by feature fields')
+    args = parser.parse_args()
+
+    main(args)
