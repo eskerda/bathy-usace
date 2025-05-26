226b498bc146adf8bbe1052058b188c99b8f2615
eskerda <eskerda@gmail.com>
2025-05-26 11:22:45 +0200

    use logger

--- totiff.py
+++ totiff.py
@@ -1,7 +1,6 @@
 import sys
+import logging
 import argparse
-
-from warnings import warn
 
 import shapely
 import rasterio
@@ -21,13 +20,15 @@ G_SMOOTH = 1.3
 # linear, cubic, nearest
 INTP_M = 'nearest'
 
+log = logging.getLogger("totiff")
+
 
 # XXX meh
 def naive_read_csv(path, * args, ** kwargs):
     with open(path) as f:
         line = f.readline()
         if ',' in line:
-            warn("%s is comma separated" % path)
+            log.warning("%s is comma separated", path)
             sep = ','
         elif ' ' in line:
             sep = r'\s+'
@@ -89,20 +90,20 @@ def main(args):
     G_SMOOTH = args.g_smooth
     files = args.files
 
-    print("Reading CSV")
+    log.info("Reading CSV")
     df = pd.concat((naive_read_csv(f, names=["x", "y", "z"]) for f in files))
     dupes = df.duplicated(subset=['x', 'y'], keep=False)
     if dupes.any():
         # XXX what should we do with duplicates, mean, min, max?
-        warn(f"Found {dupes.sum()} dupes in dataset, averaging")
+        log.warning("Found %d dupes in dataset, averaging", dupes.sum())
         df = df.groupby(['x', 'y'], as_index=False).agg({'z': 'mean'})
 
-    print("Interpolating")
+    log.info("Interpolating")
     # feet to meters
     df["z"] = df["z"] * 0.3048
-    print("x min/max:", df["x"].min(), df["x"].max())
-    print("y min/max:", df["y"].min(), df["y"].max())
-    print("z min/max:", df["z"].min(), df["z"].max())
+    log.info("x min/max: %s %s", df["x"].min(), df["x"].max())
+    log.info("y min/max: %s %s", df["y"].min(), df["y"].max())
+    log.info("z min/max: %s %s", df["z"].min(), df["z"].max())
 
     # Define grid
     xi = np.arange(df["x"].min(), df["x"].max(), GRID_RES)
@@ -114,20 +115,21 @@ def main(args):
     # Apply Gaussian smoothing
     zi = gaussian_filter(zi, sigma=G_SMOOTH)
 
+    # Mask
     if any([args.c_mask, args.d_mask]):
         if args.c_mask:
-            warn("concave mask filtering is slow, wait")
+            log.warning("concave mask filtering is slow, wait")
             mask = concave_mask(df[["x", "y"]].values, np.c_[xi.ravel(), yi.ravel()])
         elif args.d_mask:
             mask = distance_mask(df[["x", "y"]].values, np.c_[xi.ravel(), yi.ravel()])
         mask = mask.reshape(xi.shape)
-        print("Applying mask")
+        log.info("Applying mask")
         zi = np.where(mask, zi, np.nan)
 
     if args.preview:
         return preview(** locals())
 
-    print("generating tiff")
+    log.info("generating tiff")
     # Save as GeoTIFF
     xmin, xmax = xi.min(), xi.max()
     ymin, ymax = yi.min(), yi.max()
@@ -164,5 +166,12 @@ if __name__ == "__main__":
     parser.add_argument('-o', dest='outfile', help='output file')
 
     args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s.%(msecs)03d | %(levelname)s | %(message)s",
+        handlers=[logging.StreamHandler(stream=sys.stderr)],
+        datefmt='%H:%M:%S',
+    )
 
     main(args)
